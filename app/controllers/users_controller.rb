152b447b5a785b8f3afdbebda5d17213f5858015
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-08-06 19:28:34 +0900

    マイページ子供の名前表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: [:show]
+  before_action :set_child, only: [:show]
 
 def show
 end
@@ -8,5 +9,8 @@ end
 private
   def set_user
     @user = User.find(current_user.id)
+  end
+  def set_child
+    @child = Child.find(current_user.id)
   end
 end
